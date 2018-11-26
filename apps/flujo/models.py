1f28ae5567f1c9150af9f3987fc73d20298d7e44
developer <jlsuarez@raulcoka.com>
2018-11-26 17:51:19 -0500

    configuraciones

--- apps/flujo/models.py
+++ apps/flujo/models.py
@@ -10,8 +10,8 @@ class Moneda(models.Model):
 
 class Activo(models.Model):
     nombre_activo = models.CharField(max_length=255)
-    valor_activo = models.DecimalField(decimal_places=2)
-    moneda = models.ForeignKey(Moneda,on_delete=models.CASCADE())
+    valor_activo = models.DecimalField(decimal_places=2,max_digits=10)
+    moneda = models.ForeignKey(Moneda,on_delete=models.CASCADE)
     tiempo = models.CharField(max_length=30,choices=properties.TIPO_TIEMPO)
     valor_tiempo =  models.PositiveIntegerField()
 
@@ -21,24 +21,27 @@ class Acredor(models.Model):
 
 
 class Obligaciones(models.Model):
-    acredor_obligacion = models.ForeignKey(Acredor, on_delete=models.CASCADE())
-    saldo_obligacion =models.DecimalField()
-    cuota_obligacion = models.DecimalField()
+    acredor_obligacion = models.ForeignKey(Acredor, on_delete=models.CASCADE)
+    saldo_obligacion =models.DecimalField(decimal_places=2,max_digits=10)
+    cuota_obligacion = models.DecimalField(decimal_places=2,max_digits=10)
     tiempo = models.CharField(max_length=30, choices=properties.TIPO_TIEMPO)
     valor_tiempo = models.PositiveIntegerField()
     descripcion = models.CharField(max_length=500)
-    tasa_obligacion = models.DecimalField(decimal_places=2)
+    tasa_obligacion = models.DecimalField(decimal_places=2,max_digits=10)
+
 
 class Categoria(models.Model):
     tipo = models.CharField(max_length=30, choices=properties.TIPO_MOVIMIENTO)
     nombre = models.CharField(max_length=200)
 
+
 class SubCategoria(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE())
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200)
 
+
 class Movimiento(models.Model):
-    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE())
+    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
     fecha = models.DateField()
     nombre_dia = models.CharField(max_length=30, choices=properties.TIPO_DIA_SEMANA)
     nombre = models.CharField(max_length=200)
